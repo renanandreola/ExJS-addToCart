fcd3209b4f95703cfad1fea5da99b3567606eb7f
Aluno <renanandreolla@outlook.com>
2019-08-29 20:01:13 -0300

    Primeiro envio

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function addToCart (product) {
+  var cart = JSON.parse(sessionStorage.getItem("cart"));
+  if (!cart) {
+    cart = [];
+  }
+  cart.push(product);
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+}
+
+$(document).ready(function () {
+  $('form').submit(function () {
+      var productName = $('#product').val();
+      var productPrice = $('#price').val();
+      addToCart({
+        name: productName,
+        price: productPrice
+      });
+  });
+
+  var cart = sessionStorage.getItem("cart");
+  if (cart !== null) {
+    cart = JSON.parse(cart);
+    console.info("Carrinho 1 produto", cart[0].name);
+  }
+})
